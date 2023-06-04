10d92da290bcb1fb4b4cf79b071ff6daed323b9b
Artem <91053865+kostromin58@users.noreply.github.com>
2023-06-04 12:30:55 +0300

    сложение, вычитание, умножение, деление двух чисел

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите первое число: ");
+        int number2 = new Scanner(System.in).nextInt();
+
+        int firstNumber = 5;
+        int secondNumber = 10;
+        int addition = firstNumber + secondNumber;
+        System.out.println("Сумма: " + addition);
+        int subtraction = firstNumber - secondNumber;
+        System.out.println("Вычитание: " + subtraction);
+        int multiplication = firstNumber * secondNumber;
+        System.out.println("Умножение: " + multiplication);
+        double division = (double) firstNumber / secondNumber;
+        System.out.println("Деление: " + division);
+
     }
 }
